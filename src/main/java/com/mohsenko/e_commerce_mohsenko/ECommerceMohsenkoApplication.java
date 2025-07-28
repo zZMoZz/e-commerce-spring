1e20ec2e032a1002686f323819061ca3a2673e79
zmuhsenz <zmohamedmohsnz@gmail.com>
2025-07-28 17:57:29 +0300

    initiate the project

---
+++ src/main/java/com/mohsenko/e_commerce_mohsenko/ECommerceMohsenkoApplication.java
@@ -0,0 +1,13 @@
+package com.mohsenko.e_commerce_mohsenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMohsenkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMohsenkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohsenko/e_commerce_mohsenko/ECommerceMohsenkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsenko.e_commerce_mohsenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMohsenkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
