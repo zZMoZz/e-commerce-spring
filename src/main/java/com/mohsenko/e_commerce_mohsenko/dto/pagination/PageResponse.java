276db53a8161d47d86c9ef15824c07a6c583cc5c
zmuhsenz <zmohamedmohsnz@gmail.com>
2025-11-10 16:24:38 +0200

    refactor(pagination): remove @JsonProperty annotations from pagination DTOs

--- src/main/java/com/mohsenko/e_commerce_mohsenko/dto/pagination/PageResponse.java
+++ src/main/java/com/mohsenko/e_commerce_mohsenko/dto/pagination/PageResponse.java
@@ -1,13 +1,9 @@
 package com.mohsenko.e_commerce_mohsenko.dto.pagination;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PageResponse<T> extends SliceResponse<T> {
-    @JsonProperty("total_pages")
     private Integer totalPages;
-
-    @JsonProperty("total_elements")
     private Long totalElements;
 }
--- src/main/java/com/mohsenko/e_commerce_mohsenko/dto/pagination/SliceResponse.java
+++ src/main/java/com/mohsenko/e_commerce_mohsenko/dto/pagination/SliceResponse.java
@@ -1,6 +1,5 @@
 package com.mohsenko.e_commerce_mohsenko.dto.pagination;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -8,13 +7,7 @@ import java.util.List;
 @Data
 public class SliceResponse<T> {
     private List<T> content;
-
-    @JsonProperty("page_number")
     private Integer pageNumber;
-
-    @JsonProperty("page_size")
     private Integer pageSize;
-
-    @JsonProperty("has_next")
     private Boolean hasNext;
 }
